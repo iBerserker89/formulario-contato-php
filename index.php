7ddab02261c11766f075b70ba19d92a6eea3d6f9
Luciano Barros <lucianof.barros89@gmail.com>
2024-06-25 16:53:50 -0300

    enviando validacao de nome, email e comentario

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
+<?php
+    $nomeErr = $emailErr = $mensagemErr = "";
+    $nome = $email = $mensagem = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nome = test_input($_POST["nome"]);
+        $email = test_input($_POST["email"]);
+        $mensagem = test_input($_POST["mensagem"]);
+    }
+
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["nome"])) {
+            $nomeErr = 'Por favor, insira seu nome';
+        } else {
+            $nome = test_input($_POST["nome"]);
+            if (!preg_match("/^[a-zA-Z' ]*$/",$nome)) {
+                $nomeErr = 'Digite somente letras e espaços em branco';
+            }
+        }
+
+        if (empty($_POST["email"])) {
+            $emailErr = 'Por favor, insira seu email';
+        } else {
+            $email = test_input($_POST["email"]);
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $emailErr = 'Formato de email inválido';
+            }
+        }
+
+        if (empty($_POST["mensagem"])) {
+            $mensagemErr = 'Por favor, insira sua mensagem';
+        } else {
+            $mensagem = test_input($_POST["mensagem"]);
+        }
+    }
+?>
+
 <!doctype html>
 <html lang="pt-br">
 <head>
@@ -9,64 +53,23 @@
 <body>
     <h1>Preencha os Campos Abaixo</h1>
     <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        Digite seu nome:
-        <label for="nome">
-            <input type="text" name="nome">
+        <label for="nome">Digite seu nome:
+            <input type="text" name="nome" value="<?php echo $nome; ?>" >
         </label>
-        <span>* <?php global $nomeErr; echo $nomeErr; ?></span>
+        <span class="erro">* <?php echo $nomeErr; ?></span>
         <br><br>
         <label for="email">Digite seu email:
-            <input type="email" name="email">
+            <input type="email" name="email" value="<?php echo $email; ?>" >
         </label>
-        <span>* <?php global $emailErr; echo $emailErr; ?></span>
+        <span class="erro">* <?php  echo $emailErr; ?></span>
         <br><br>
         <label for="mensagem">Digite sua mensagem: </label>
-        <br>
-        <textarea name="mensagem" id="mensagem" cols="30" rows="10"></textarea>
-        <span>* <?php global $mensagemErr; echo $mensagemErr; ?></span>
-
+        <textarea name="mensagem" id="mensagem" cols="40" rows="5"><?php echo $mensagem; ?></textarea>
+        <span class="erro">* <?php  echo $mensagemErr; ?></span>
         <br><br>
         <input type="submit" value="Enviar">
     </form>
-
-    <?php
-        $nomeErr = $emailErr = $mensagemErr = "";
-        $nome = $email = $mensagem = "";
-
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $nome = test_input($_POST["nome"]);
-            $email = test_input($_POST["email"]);
-            $mensagem = test_input($_POST["mensagem"]);
-        }
-
-        function test_input($data) {
-            $data = trim($data);
-            $data = stripslashes($data);
-            $data = htmlspecialchars($data);
-            return $data;
-        }
-
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            if (empty($_POST["nome"])) {
-                $nomeErr = 'Por favor, insira seu nome';
-            } else {
-                $nome = test_input($_POST["nome"]);
-            }
-
-            if (empty($_POST["email"])) {
-                $emailErr = 'Por favor, insira seu email';
-            } else {
-                $email = test_input($_POST["email"]);
-            }
-
-            if (empty($_POST["mensagem"])) {
-                $mensagemErr = 'Por favor, insira sua mensagem';
-            } else {
-                $mensagem = test_input($_POST["mensagem"]);
-            }
-        }
-        
-    ?>
-
 </body>
 </html>
+
+
